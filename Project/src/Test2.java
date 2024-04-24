517ab26340ba0edaf0ee2ab7b957242b5b7582b2
gimhong-gi <khg1830@naver.com>
2024-04-24 22:35:54 +0900

    LogIn class 제작 Test2 수정

--- Project/src/Test2.java
+++ Project/src/Test2.java
@@ -1,10 +1,9 @@
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import javax.swing.*;
 
 public class Test2 {
 
@@ -13,12 +12,34 @@ public class Test2 {
 		JPanel panel = new JPanel();
 		JLabel label = new JLabel("Some text");
 		JButton btn1 = new JButton("Click me!!");
+		JButton btn2 = new JButton("Exit");
+		JTextArea txtArea = new JTextArea();
+		JPanel btnPanel = new JPanel();
+
 		
 		panel.setLayout(new BorderLayout());
-		
-		panel.add(new JLabel("Welcome this lecture"));
-		panel.add(btn1, BorderLayout.WEST);
-		
+
+		btnPanel.add(btn1);
+		btnPanel.add(btn2);
+		panel.add(label, BorderLayout.NORTH);
+		panel.add(btnPanel, BorderLayout.WEST);
+		panel.add(txtArea, BorderLayout.CENTER);
+
+		btn1.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				label.setText(txtArea.getText());
+			}
+		});
+
+		btn2.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+
 		frame.add(panel);
 		
 		frame.setResizable(false);
