7a98db6782193721a25bc3155e13367cfd743830
21-manic-020g <21-manic-020g@21-manic-020>
2024-04-24 17:44:55 +0900

    김홍기 test2 사용중

--- Project/src/Test2.java
+++ Project/src/Test2.java
@@ -1,9 +1,32 @@
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class Test2 {
 
 	public static void main(String[] args) {
-		System.out.println("테스트2 입니다.");
-
-	}
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		JLabel label = new JLabel("Some text");
+		JButton btn1 = new JButton("Click me!!");
+		
+		panel.setLayout(new BorderLayout());
+		
+		panel.add(new JLabel("Welcome this lecture"));
+		panel.add(btn1, BorderLayout.WEST);
+		
+		frame.add(panel);
+		
+		frame.setResizable(false);
+		frame.setVisible(true);
+		frame.setPreferredSize(new Dimension(840, 840/12*9));
+		frame.setSize(840, 840/12*9);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		}
 
 }
